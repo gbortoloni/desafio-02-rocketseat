2ba740b8a5033b918aed715f28fc3ac5432ca7dd
Giuliano Bortoloni <giulianoagria@hotmail.com>
2020-07-10 14:40:42 -0300

    delete repository

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
@@ -35,7 +37,9 @@ function App() {
         {repositories.map((repository) => (
           <li key={repository.id}>
             {repository.title}
-            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
